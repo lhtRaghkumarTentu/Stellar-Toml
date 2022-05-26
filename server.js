aa412e0ab4bef2501e550dd281b90c643e5ab525
RaghukumarTentu <tentu@leewayhertz.com>
2022-05-26 09:37:01 +0530

    Little Changes

--- server.js
+++ server.js
@@ -2,19 +2,22 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 8000;
 const path = require('path');
+const cors = require('cors');
 
 app.get("/",(req,res)=>{
     res.send("Hello EveyOne!!!")
-})
+});
+
+app.use(cors({
+  origin: '*'
+}));
 
 app.get('/.wellknown/stellar.toml', (req, res, next) => {
     const options = {
       root: path.join(__dirname, 'public'),
-      headers: {
-        "content-type": "text/plain",
-        "Access-Control-Allow-Origin": "*"
-      }
     }
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("content-type", "text/plain");
     res.sendFile('stellar.txt', options);
   })
 
